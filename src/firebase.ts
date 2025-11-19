8bf76a869cb2f8c0ad0864a13aa2e40f9e674fcb
Tilek Erkebaev <108805434+Tilekdevs@users.noreply.github.com>
2025-11-19 14:29:06 +0600

    test fix deploy

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,22 +1,27 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
+import { initializeApp } from 'firebase/app'
+import {
+	getAuth,
+	GoogleAuthProvider,
+	onAuthStateChanged,
+	signInWithPopup,
+	signOut,
+} from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-};
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_FIREBASE_APP_ID,
+}
+console.log("API KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
 
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
-export const loginWithGoogle = () => signInWithPopup(auth, googleProvider);
-export const logout = () => signOut(auth);
-export const listenUser = (callback: (user: any) => void) => onAuthStateChanged(auth, callback);
-
-export const db = getFirestore(app);
+const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
+export const googleProvider = new GoogleAuthProvider()
+export const loginWithGoogle = () => signInWithPopup(auth, googleProvider)
+export const logout = () => signOut(auth)
+export const listenUser = (callback: (user: any) => void) =>
+	onAuthStateChanged(auth, callback)
+export const db = getFirestore(app)
